b3e927ed1a9feedb73461c75f89df4f4fd9b5bdd
Madhumithra <mithramadhu005@gmail.com>
2024-09-29 18:12:36 +0530

    Arrays

---
+++ Arrays/Problem16.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int closestSum = nums[0]+nums[1]+nums[2];
+        for(int i = 0;i<n-2;i++){
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int presentSum = nums[i]+nums[left]+nums[right];
+                if(presentSum==target){
+                    return presentSum;
+                }
+                if(Math.abs(presentSum-target)<Math.abs(closestSum-target)){
+                    closestSum = presentSum;
+                }
+                if(presentSum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return closestSum;
+    }
+}
