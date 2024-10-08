99bc55bec9d6ba0a1f8012f128952d00da60f564
Madhumithra <mithramadhu005@gmail.com>
2024-10-08 15:47:40 +0530

    [add]ArrayLists

---
+++ HashTable/Problem409.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int length = 0;
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c); 
+                length += 2;   
+            } else {
+                set.add(c);    
+            }
+        }
+
+        if (!set.isEmpty()) {
+            length += 1;
+        }
+
+        return length;
+    }
+        }
